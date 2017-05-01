d578727877d3ccd167245ae2315ea7f3ebfecf9d
Richard O'Brien <rich0134o@hotmail.com>
2017-05-01 20:58:46 +0100

    Initial commit

---
+++ src/components/SequentialLayCalculator.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="sequential-lay-calculator">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h4>{{ msg }}</h4>
+      </div>
+      <div class="panel-body">
+        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sequential-lay-calculator',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .sequential-lay-calculator {
+    padding-top: 65px;
+  }
+</style>
