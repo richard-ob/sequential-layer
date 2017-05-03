17746cec06f1619ce15628cf220fd29ee36d611b
Richard O'Brien <rich0134o@hotmail.com>
2017-05-03 21:50:20 +0100

    Added initially required methods for add/remove legs

--- src/components/SequentialLayCalculator.vue
+++ src/components/SequentialLayCalculator.vue
@@ -1,11 +1,21 @@
 <template>
   <div class="sequential-lay-calculator">
-    <div class="panel panel-default">
+    <div class="panel panel-info">
       <div class="panel-heading">
-        <h4>{{ msg }}</h4>
+        <h4>Sequential Lay Calculator</h4>
       </div>
       <div class="panel-body">
-        
+        <form>
+          <div class="form-group" v-for="leg in legs">
+            <label>Odds</label>
+            <input type="text" v-model="leg.odds"/>
+            <label>Stake</label>
+            <input type="text" v-model="leg.stake" disabled/>
+            <button type="button" class="btn btn-danger" v-on:click="removeLeg(leg)"><i class="glyphicon glyphicon-remove"></i></button>
+          </div>
+        <button type="button" class="btn btn-info" v-on:click="addLeg"><i class="glyphicon glyphicon-plus"></i> Add Leg</button>
+        <button type="button" class="btn btn-success" v-on:click="calculate"><i class="glyphicon glyphicon-refresh"></i> Calculate</button>
+        </form>
       </div>
     </div>
   </div>
@@ -16,13 +26,27 @@ export default {
   name: 'sequential-lay-calculator',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      legs: [
+        {odds: 1.5, stake: 0},
+        {odds: 1.5, stake: 0},
+        {odds: 2, stake: 0}
+      ]
+    }
+  },
+  methods: {
+    addLeg: function () {
+      this.legs.push({odds: 1.5, stake: 0})
+    },
+    removeLeg: function (leg) {
+      this.legs = this.legs.filter(l => l !== leg)
+    },
+    calculate: function () {
+      this.legs[0].odds = 9
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .sequential-lay-calculator {
     padding-top: 65px;
