cdf3186ad4a0903d4e44f1ecac34fbc94bd03b95
Richard O'Brien <richard.obrien4@gmail.com>
2018-09-30 23:54:29 +0100

    Remove unnecessary navigation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,7 @@
 <template>
-  <div id="app">
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false"
-            aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Sequential Layer</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
     <div class="container">
       <sequential-lay-calculator></sequential-lay-calculator>
     </div>
-  </div>
 </template>
 
 <script>
